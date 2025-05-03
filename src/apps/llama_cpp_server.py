286883d7aeba149a2438c539b23e360791b2e9d0
Baptiste Bouffaut <baptiste.bouffaut@gmail.com>
2025-05-03 23:20:57 +0200

    WIP DEBUG app triggered twice

--- src/apps/llama_cpp_server.py
+++ src/apps/llama_cpp_server.py
@@ -1,35 +1,11 @@
-import os
-from pathlib import Path
 
 from sanic import Sanic
-
-from src.adapters.model_adapter import ModelAdapter
-from src.ports.model_port_llama_cpp import LlamaCppModel
 from src.services.sanic_server import create_server
-from src.utils import logger
-from src.utils.constants import MODEL_FILE, MODEL_PATH
 
 
-def create_app(model_file_path: str) -> Sanic:
-    
-    # Ensure the model file exists
-    if not Path(model_file_path).exists():
-        logger.error(f"Error: Model file not found at {model_file_path}")
-        exit(1)
-    
-    # Initialize the model with the specified path
-    llama_cpp_model: ModelAdapter = LlamaCppModel(model_path=model_file_path)
-    
-    # Create and return the Sanic app
-    app = create_server(llama_cpp_model)
-    return app
+def main() -> Sanic:
 
-def main():
-
- # Set model path
-    model_file_path = os.path.join(MODEL_PATH, MODEL_FILE)
-
-    app = create_app(model_file_path)
+    app = create_server()
     return app
 
 app = main()
--- src/services/sanic_server.py
+++ src/services/sanic_server.py
@@ -1,7 +1,12 @@
+import os
+from pathlib import Path
 from sanic import Sanic
 
 from src.adapters.model_adapter import ModelAdapter
+from src.ports.model_port_llama_cpp import LlamaCppModel
 from src.services.openai_v1_blueprint import openai_v1_bp
+from src.utils.logger import logger
+from src.utils.constants import MODEL_FILE, MODEL_PATH
 
 
 def create_server(model_adapter: ModelAdapter) -> Sanic:
@@ -13,6 +18,14 @@ def create_server(model_adapter: ModelAdapter) -> Sanic:
     # Set mock model in app context
     @app.before_server_start
     async def setup_model(app):
-        app.ctx.model = model_adapter
+        model_file_path = os.path.join(MODEL_PATH, MODEL_FILE)
+        # Ensure the model file exists
+        if not Path(model_file_path).exists():
+            logger.error(f"Error: Model file not found at {model_file_path}")
+            exit(1)
+        
+        # Initialize the model with the specified path
+        llama_cpp_model: ModelAdapter = LlamaCppModel(model_path=model_file_path)
+        app.ctx.model = llama_cpp_model
 
     return app
