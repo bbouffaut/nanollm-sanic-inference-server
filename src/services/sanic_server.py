caaa64923620c9c5d17086adc42ab0001b518066
Baptiste Bouffaut <baptiste.bouffaut@gmail.com>
2025-05-03 12:28:56 +0200

    Add listening port for sanic

--- src/services/sanic_server.py
+++ src/services/sanic_server.py
@@ -3,11 +3,13 @@ from sanic import Sanic
 
 from src.adapters.model_adapter import ModelAdapter
 from src.services.openai_v1_blueprint import openai_v1_bp
+from src.utils.constants import SERVER_PORT
 
 
 def create_server(model_adapter: ModelAdapter) -> Sanic:
     
     app = Sanic(model_adapter.name)
+    app.prepare(port=SERVER_PORT)
     bp = openai_v1_bp
     app.blueprint(bp)
 
