08543265483079dfb3ab88e5e483901e3596ef81
bbouffaut <baptiste.bouffaut@gmail.com>
2025-05-08 14:53:25 +0200

    Save docker image into github skails repository

--- docker/build_arm_docker.sh
+++ docker/build_arm_docker.sh
@@ -3,4 +3,4 @@
 TAG=$1
 PLATFORM=$2
 
-docker buildx build --platform $PLATFORM -t venv-3.12-llama_cpp-cuda:$TAG -f ./docker/Dockerfile .
+docker buildx build --push --platform $PLATFORM -t registry.gitlab.com/skails/jetson-nano/venv-3.12-llama_cpp-cuda:$TAG -f ./docker/Dockerfile .
