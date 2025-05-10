762236e842e859cdad54d0e88b3f95f5f116f0ff
bbouffaut <baptiste.bouffaut@gmail.com>
2025-05-10 16:13:35 +0200

    Remove transformers since this is NOK

--- src/types/models_classes.py
+++ src/types/models_classes.py
@@ -2,7 +2,6 @@ from src.ports.model_port_echo import EchoModel
 from src.ports.model_port_echo_with_tokenizer import EchoModelWithTokenizer
 from src.ports.model_port_llama_cpp import LlamaCppModel
 from src.ports.model_port_mock import MockModel
-from src.ports.model_port_transformers import TransformersModel
 
 
 class ModelClasses:
@@ -10,7 +9,6 @@ class ModelClasses:
     EchoModelWithTokenizer = EchoModelWithTokenizer
     EchoModel = EchoModel
     MockModel = MockModel
-    TransformersModel = TransformersModel
 
     def get(self, name, default=None):
         return getattr(self, name, default)
