6d5e6131e57807e212c841054764540f7b73a705
bbouffaut <baptiste.bouffaut@gmail.com>
2025-05-10 18:37:23 +0200

    Dockerfile optimized

--- docker/build_arm_docker.sh
+++ docker/build_arm_docker.sh
@@ -7,4 +7,4 @@ PLATFORM=$2
 # docker buildx build --push --platform $PLATFORM -t registry.gitlab.com/skails/jetson-nano/venv-3.10-llama_cpp-nanollm-cuda:$TAG -f ./docker/Dockerfile .
 
 # llama-cpp + mlc_llm
-docker buildx build --push --platform $PLATFORM -t registry.gitlab.com/skails/jetson-nano/venv-3.10-llama_cpp-mlc-cuda:$TAG -f ./docker/Dockerfile .
+docker buildx build --push --platform $PLATFORM -t registry.gitlab.com/skails/jetson-nano/venv-3.10-llama_cpp-nanollm-cuda:$TAG -f ./docker/Dockerfile .
