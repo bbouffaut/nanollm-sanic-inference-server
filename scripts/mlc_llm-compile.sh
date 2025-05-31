35ebe28d410646a203e2db604b3b9bd859656c31
bbouffaut <baptiste.bouffaut@gmail.com>
2025-06-01 00:40:58 +0200

    Compile Gemma2

--- scripts/mlc_llm-compile.sh
+++ scripts/mlc_llm-compile.sh
@@ -1,5 +1,11 @@
 # update /root/.cache/mlc_llm/mlc-ai/gemma-2-2b-it-q4f16_1-MLC/mlc_llm-chat.config Before compiling
+
 mlc_llm compile --device cuda --opt O3 \
     --output /data/models/mlc_llm/model_lib/gemma-2-2b-it-q4f16_1-mlc_aarch64-cu126-sm87.so \
-    --overrides='tensor_parallel_shards=1;context_window_size=8192;prefill_chunk_size=4096' \
+    --overrides="tensor_parallel_shards=1;context_window_size=8192;prefill_chunk_size=4096" \
     /data/models/mlc_llm/mlc-ai/gemma-2-2b-it-q4f16_1-MLC/mlc-chat-config.json
+
+#mlc_llm compile --device cuda --opt O3 \
+#    --output /data/models/mlc_llm/model_lib/llama-3.1-8b-instruct-q4f16_ft-mlc-aarch64-cu126-sm87.so \
+#    --overrides="tensor_parallel_shards=1;context_window_size=8192;prefill_chunk_size=4096" \
+#    /data/models/mlc_llm/dusty-nv/Llama-3.1-8B-Instruct-q4f16_ft-MLC/mlc-chat-config.json
