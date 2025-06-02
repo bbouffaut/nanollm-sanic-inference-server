32ce054533aa490124f3451cb8d01a27abd80420
bbouffaut <baptiste.bouffaut@gmail.com>
2025-06-03 00:18:11 +0200

    WIP: try to use Qwen 2.5 code, but not enough memory

--- scripts/mlc_llm-compile.sh
+++ scripts/mlc_llm-compile.sh
@@ -1,11 +1,16 @@
 # update /root/.cache/mlc_llm/mlc-ai/gemma-2-2b-it-q4f16_1-MLC/mlc_llm-chat.config Before compiling
 
-mlc_llm compile --device cuda --opt O3 \
-    --output /data/models/mlc_llm/model_lib/gemma-2-2b-it-q4f16_1-mlc_aarch64-cu126-sm87.so \
-    --overrides="tensor_parallel_shards=1;context_window_size=8192;prefill_chunk_size=4096" \
-    /data/models/mlc_llm/mlc-ai/gemma-2-2b-it-q4f16_1-MLC/mlc-chat-config.json
+#mlc_llm compile --device cuda --opt O3 \
+#    --output /data/models/mlc_llm/model_lib/gemma-2-2b-it-q4f16_1-mlc_aarch64-cu126-sm87.so \
+#    --overrides="tensor_parallel_shards=1;context_window_size=8192;prefill_chunk_size=4096" \
+#    /data/models/mlc_llm/mlc-ai/gemma-2-2b-it-q4f16_1-MLC/mlc-chat-config.json
 
 #mlc_llm compile --device cuda --opt O3 \
 #    --output /data/models/mlc_llm/model_lib/llama-3.1-8b-instruct-q4f16_ft-mlc-aarch64-cu126-sm87.so \
 #    --overrides="tensor_parallel_shards=1;context_window_size=8192;prefill_chunk_size=4096" \
 #    /data/models/mlc_llm/dusty-nv/Llama-3.1-8B-Instruct-q4f16_ft-MLC/mlc-chat-config.json
+
+mlc_llm compile --device cuda --opt O3 \
+    --output /data/models/mlc_llm/model_lib/qwen2.5-coder-7b-instruct_aarch64-cu126-sm87_8192-context-window.so \
+    --overrides="tensor_parallel_shards=1;context_window_size=8192;prefill_chunk_size=4096" \
+    /data/models/mlc_llm/Qwen/Qwen2.5-Coder-7B-Instruct-q4f16_ft-MLC/mlc-chat-config.json
